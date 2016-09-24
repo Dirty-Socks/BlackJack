d07f223a59fbd33286a6443f55b9782322e70277
Ian Young <ianyoung3737@gmail.com>
2016-09-24 15:45:15 -0400

    added Card CompareTo

--- Homework1/Card.cs
+++ Homework1/Card.cs
@@ -5,8 +5,28 @@ namespace Homework1
 {
 	public class Card
 	{
-		public Card ()
+		private Suit s;
+		private Rank r;
+
+		public Card (Suit suit, Rank rank)
 		{
+			s = suit;
+			r = rank;
+		}
+
+		public int CompareTo (Card OtherCardObject)
+		{
+			if (this.s.CompareTo (OtherCardObject.s) == 0 && this.r.CompareTo (OtherCardObject.r) == 0)
+				return 0;
+			else if (this.r.CompareTo (OtherCardObject.r) == -1)
+				return -1;
+			else
+				return 1;
+		}
+
+		public static void Main ()
+		{
+			//Card c = new Card (Suit.CLUBS, Rank.ACE);
 		}
 	}
 }
