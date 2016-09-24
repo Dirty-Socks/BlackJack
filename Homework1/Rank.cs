779846af3b94488f374f5d267c2b678bb6c72f59
Ian Young <ianyoung3737@gmail.com>
2016-09-24 15:14:26 -0400

    second commit

---
+++ Homework1/Rank.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework1
+{
+	public class Rank
+	{
+		public string _name;
+		public string _symbol;
+
+		public static Rank TWO;
+		public static Rank THREE;
+		public static Rank FOUR;
+		public static Rank FIVE;
+		public static Rank SIX;
+		public static Rank SEVEN;
+		public static Rank EIGHT;
+		public static Rank NINE;
+		public static Rank TEN;
+		public static Rank JACK;
+		public static Rank QUEEN;
+		public static Rank KING;
+		public static Rank ACE;
+
+		public static List<Rank> VALUES = new List<Rank>();
+
+		static Rank ()
+		{
+			TWO = new Rank("Two","2");
+			THREE = new Rank("Three","3");
+			FOUR = new Rank("Four","4");
+			FIVE = new Rank ("Five", "5");
+			SIX = new Rank("Six","6");
+			SEVEN = new Rank("Seven","7");
+			EIGHT = new Rank("Eight","8");
+			NINE = new Rank("Nine","9");
+			TEN = new Rank("Ten","10");
+			JACK = new Rank("Jack","J");
+			QUEEN = new Rank("Queen","Q");
+			KING = new Rank("King","K");
+			ACE = new Rank("Ace","A");
+
+			VALUES.Add (TWO);
+			VALUES.Add (THREE);
+			VALUES.Add (FOUR);
+			VALUES.Add (FIVE);
+			VALUES.Add (SIX);
+			VALUES.Add (SEVEN);
+			VALUES.Add (EIGHT);
+			VALUES.Add (NINE);
+			VALUES.Add (TEN);
+			VALUES.Add (JACK);
+			VALUES.Add (QUEEN);
+			VALUES.Add (KING);
+			VALUES.Add (ACE);
+		}
+
+		public Rank (string nam, string sym)
+		{
+			_name = nam;
+			_symbol = sym;
+		}
+
+		public int CompareTo (Rank OtherRankObject)
+		{
+			int objA = VALUES.IndexOf (this);
+			int objB = VALUES.IndexOf (OtherRankObject);
+
+			if (objA == objB)
+				return 0;
+			else if (objA < objB)
+				return -1;
+			else
+				return 1;
+		}
+
+		public string GetSymbol ()
+		{
+			return this._symbol;
+		}
+
+		public string GetName()
+		{
+			return this._name;
+		}
+
+		public override string ToString()
+		{
+			return this._name;
+		}
+	}
+}
+
---
+++ Homework1/Suit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework1
+{
+	public class Suit
+	{
+
+		public static string _name;
+		public static string _symbol;
+
+		public static Suit CLUBS;
+		public static Suit DIAMONDS;
+		public static Suit SPADES;
+		public static Suit HEARTS;
+
+		public static List<Suit> VALUES = new List<Suit>();
+
+		static Suit ()
+		{
+			CLUBS = new Suit ("Clubs", "C");
+			DIAMONDS = new Suit ("Diamonds", "D");
+			HEARTS = new Suit ("HEARTS", "H");
+			SPADES = new Suit ("Spades", "S");
+		}
+
+		public Suit	(string nam, string sym)
+		{
+			_name = nam;
+			_symbol = sym;
+		}
+
+		public int CompareTo (Suit OtherSuitObject)
+		{
+			if (VALUES.IndexOf (this) == VALUES.IndexOf (OtherSuitObject))
+				return 0;
+			else
+				return -1;
+		}
+
+		public string GetSymbol ()
+		{
+			return _symbol;
+		}
+
+		public string GetName ()
+		{
+			return _name;
+		}
+
+		public override string ToString ()
+		{
+			return _name;
+		}
+	}
+}
+
