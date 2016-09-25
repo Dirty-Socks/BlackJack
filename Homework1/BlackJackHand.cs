b75728b65d82d153d7678dd3e8919fe8e35a0634
Ian Young <ianyoung3737@gmail.com>
2016-09-25 17:41:07 -0400

    Added blackjackhand

--- Homework1/BlackJackHand.cs
+++ Homework1/BlackJackHand.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 
 namespace Homework1
 {
-	public class CardCountHand : Hand
+	public class BlackJackHand : Hand
 	{
-
-		public CardCountHand ()
+		public BlackJackHand ()
 		{
 		}
 
@@ -46,46 +45,46 @@ namespace Homework1
 			foreach (Card crd in this.VALUES) {
 				string sym = crd.GetRank ().GetSymbol ();
 				switch (sym) {
-					case "2":
-						value += 2;
-						break;
-					case "3":
-						value += 3;
-						break;
-					case "4":
-						value += 4;
-						break;
-					case "5":
-						value += 5;
-						break;
-					case "6":
-						value += 6;
-						break;
-					case "7":
-						value += 7;
-						break;
-					case "8":
-						value += 8;
-						break;
-					case "9":
-						value += 9;
-						break;
-					case "10":
-						value += 10;
-						break;
-					case "J":
-						value += 10;
-						break;
-					case "Q":
-						value += 10;
-						break;
-					case "K":
-						value += 10;
-						break;
-					case "A":
-						value += 1;
-						break;
-					}
+				case "2":
+					value += 2;
+					break;
+				case "3":
+					value += 3;
+					break;
+				case "4":
+					value += 4;
+					break;
+				case "5":
+					value += 5;
+					break;
+				case "6":
+					value += 6;
+					break;
+				case "7":
+					value += 7;
+					break;
+				case "8":
+					value += 8;
+					break;
+				case "9":
+					value += 9;
+					break;
+				case "10":
+					value += 10;
+					break;
+				case "J":
+					value += 10;
+					break;
+				case "Q":
+					value += 10;
+					break;
+				case "K":
+					value += 10;
+					break;
+				case "A":
+					value += 11;
+					break;
+				}
 			}
 			return value;
 		}
