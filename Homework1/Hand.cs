cd5adbcf208bfc3fc7f9bcf8dda2c44cba2bcd7b
Ian Young <ianyoung3737@gmail.com>
2016-09-25 12:17:59 -0400

    added list in hand

--- Homework1/Hand.cs
+++ Homework1/Hand.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Homework1
 {
 	abstract class Hand
 	{
+
+		private List<Card> hand;
+
+ 
 		public Hand ()
 		{
+			hand = new List<Card> ();
 		}
+
 	}
 }
 
