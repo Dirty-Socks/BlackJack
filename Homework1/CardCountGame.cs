323b44fdf26f1e3a87885e564eeacd7a4d84e2f1
Ian Young <ianyoung3737@gmail.com>
2016-09-25 20:15:14 -0400

    added cards to deck- kill me there has to be a way to loop this

--- Homework1/CardCountGame.cs
+++ Homework1/CardCountGame.cs
@@ -12,61 +12,121 @@ namespace Homework1
 		{
 			Deck d = new Deck ();
 
-			Card c14 = new Card (Suit.CLUBS, Rank.ACE);
-			Card c13 = new Card (Suit.CLUBS, Rank.KING);
-			Card c12 = new Card (Suit.CLUBS, Rank.QUEEN);
-			Card c11 = new Card (Suit.CLUBS, Rank.JACK);
-			Card c10 = new Card (Suit.CLUBS, Rank.TEN);
-			Card c9 = new Card (Suit.CLUBS, Rank.NINE);
-			Card c8 = new Card (Suit.CLUBS, Rank.EIGHT);
-			Card c7 = new Card (Suit.CLUBS, Rank.SEVEN);
-			Card c6 = new Card (Suit.CLUBS, Rank.SIX);
-			Card c5 = new Card (Suit.CLUBS, Rank.FIVE);
-			Card c4 = new Card (Suit.CLUBS, Rank.FOUR);
-			Card c3 = new Card (Suit.CLUBS, Rank.THREE);
-			Card c2 = new Card (Suit.CLUBS, Rank.TWO);
+			Card c13 = new Card (Suit.CLUBS, Rank.ACE);
+			Card c12 = new Card (Suit.CLUBS, Rank.KING);
+			Card c11 = new Card (Suit.CLUBS, Rank.QUEEN);
+			Card c10 = new Card (Suit.CLUBS, Rank.JACK);
+			Card c9 = new Card (Suit.CLUBS, Rank.TEN);
+			Card c8 = new Card (Suit.CLUBS, Rank.NINE);
+			Card c7 = new Card (Suit.CLUBS, Rank.EIGHT);
+			Card c6 = new Card (Suit.CLUBS, Rank.SEVEN);
+			Card c5 = new Card (Suit.CLUBS, Rank.SIX);
+			Card c4 = new Card (Suit.CLUBS, Rank.FIVE);
+			Card c3 = new Card (Suit.CLUBS, Rank.FOUR);
+			Card c2 = new Card (Suit.CLUBS, Rank.THREE);
+			Card c1 = new Card (Suit.CLUBS, Rank.TWO);
 
-			Card c27 = new Card (Suit.DIAMONDS, Rank.ACE);
-			Card c26 = new Card (Suit.DIAMONDS, Rank.KING);
-			Card c25 = new Card (Suit.DIAMONDS, Rank.QUEEN);
-			Card c24 = new Card (Suit.DIAMONDS, Rank.JACK);
-			Card c23 = new Card (Suit.DIAMONDS, Rank.TEN);
-			Card c22 = new Card (Suit.DIAMONDS, Rank.NINE);
-			Card c21 = new Card (Suit.DIAMONDS, Rank.EIGHT);
-			Card c20 = new Card (Suit.DIAMONDS, Rank.SEVEN);
-			Card c19 = new Card (Suit.DIAMONDS, Rank.SIX);
-			Card c18 = new Card (Suit.DIAMONDS, Rank.FIVE);
-			Card c17 = new Card (Suit.DIAMONDS, Rank.FOUR);
-			Card c16 = new Card (Suit.DIAMONDS, Rank.THREE);
-			Card c15 = new Card (Suit.DIAMONDS, Rank.TWO);
+			Card c26 = new Card (Suit.DIAMONDS, Rank.ACE);
+			Card c25 = new Card (Suit.DIAMONDS, Rank.KING);
+			Card c24 = new Card (Suit.DIAMONDS, Rank.QUEEN);
+			Card c23 = new Card (Suit.DIAMONDS, Rank.JACK);
+			Card c22 = new Card (Suit.DIAMONDS, Rank.TEN);
+			Card c21 = new Card (Suit.DIAMONDS, Rank.NINE);
+			Card c20 = new Card (Suit.DIAMONDS, Rank.EIGHT);
+			Card c19 = new Card (Suit.DIAMONDS, Rank.SEVEN);
+			Card c18 = new Card (Suit.DIAMONDS, Rank.SIX);
+			Card c17 = new Card (Suit.DIAMONDS, Rank.FIVE);
+			Card c16 = new Card (Suit.DIAMONDS, Rank.FOUR);
+			Card c15 = new Card (Suit.DIAMONDS, Rank.THREE);
+			Card c14 = new Card (Suit.DIAMONDS, Rank.TWO);
 
-			Card c40 = new Card (Suit.HEARTS, Rank.ACE);
-			Card c39 = new Card (Suit.HEARTS, Rank.KING);
-			Card c38 = new Card (Suit.HEARTS, Rank.QUEEN);
-			Card c37 = new Card (Suit.HEARTS, Rank.JACK);
-			Card c36 = new Card (Suit.HEARTS, Rank.TEN);
-			Card c35 = new Card (Suit.HEARTS, Rank.NINE);
-			Card c34 = new Card (Suit.HEARTS, Rank.EIGHT);
-			Card c33 = new Card (Suit.HEARTS, Rank.SEVEN);
-			Card c32 = new Card (Suit.HEARTS, Rank.SIX);
-			Card c31 = new Card (Suit.HEARTS, Rank.FIVE);
-			Card c30 = new Card (Suit.HEARTS, Rank.FOUR);
-			Card c29 = new Card (Suit.HEARTS, Rank.THREE);
-			Card c28 = new Card (Suit.HEARTS, Rank.TWO);
+			Card c39 = new Card (Suit.HEARTS, Rank.ACE);
+			Card c38 = new Card (Suit.HEARTS, Rank.KING);
+			Card c37 = new Card (Suit.HEARTS, Rank.QUEEN);
+			Card c36 = new Card (Suit.HEARTS, Rank.JACK);
+			Card c35 = new Card (Suit.HEARTS, Rank.TEN);
+			Card c34 = new Card (Suit.HEARTS, Rank.NINE);
+			Card c33 = new Card (Suit.HEARTS, Rank.EIGHT);
+			Card c32 = new Card (Suit.HEARTS, Rank.SEVEN);
+			Card c31 = new Card (Suit.HEARTS, Rank.SIX);
+			Card c30 = new Card (Suit.HEARTS, Rank.FIVE);
+			Card c29 = new Card (Suit.HEARTS, Rank.FOUR);
+			Card c28 = new Card (Suit.HEARTS, Rank.THREE);
+			Card c27 = new Card (Suit.HEARTS, Rank.TWO);
 
-			Card c53 = new Card (Suit.SPADES, Rank.ACE);
-			Card c52 = new Card (Suit.SPADES, Rank.KING);
-			Card c51 = new Card (Suit.SPADES, Rank.QUEEN);
-			Card c50 = new Card (Suit.SPADES, Rank.JACK);
-			Card c49 = new Card (Suit.SPADES, Rank.TEN);
-			Card c48 = new Card (Suit.SPADES, Rank.NINE);
-			Card c47 = new Card (Suit.SPADES, Rank.EIGHT);
-			Card c46 = new Card (Suit.SPADES, Rank.SEVEN);
-			Card c45 = new Card (Suit.SPADES, Rank.SIX);
-			Card c44 = new Card (Suit.SPADES, Rank.FIVE);
-			Card c43 = new Card (Suit.SPADES, Rank.FOUR);
-			Card c42 = new Card (Suit.SPADES, Rank.THREE);
-			Card c41 = new Card (Suit.SPADES, Rank.TWO);
+			Card c52 = new Card (Suit.SPADES, Rank.ACE);
+			Card c51 = new Card (Suit.SPADES, Rank.KING);
+			Card c50 = new Card (Suit.SPADES, Rank.QUEEN);
+			Card c49 = new Card (Suit.SPADES, Rank.JACK);
+			Card c48 = new Card (Suit.SPADES, Rank.TEN);
+			Card c47 = new Card (Suit.SPADES, Rank.NINE);
+			Card c46 = new Card (Suit.SPADES, Rank.EIGHT);
+			Card c45 = new Card (Suit.SPADES, Rank.SEVEN);
+			Card c44 = new Card (Suit.SPADES, Rank.SIX);
+			Card c43 = new Card (Suit.SPADES, Rank.FIVE);
+			Card c42 = new Card (Suit.SPADES, Rank.FOUR);
+			Card c41 = new Card (Suit.SPADES, Rank.THREE);
+			Card c40 = new Card (Suit.SPADES, Rank.TWO);
+
+			d.AddCard (c1);
+			d.AddCard (c2);
+			d.AddCard (c3);
+			d.AddCard (c4);
+			d.AddCard (c5);
+			d.AddCard (c6);
+			d.AddCard (c7);
+			d.AddCard (c8);
+			d.AddCard (c9);
+			d.AddCard (c10);
+			d.AddCard (c11);
+			d.AddCard (c12);
+			d.AddCard (c13);
+			d.AddCard (c14);
+			d.AddCard (c15);
+			d.AddCard (c16);
+			d.AddCard (c17);
+			d.AddCard (c18);
+			d.AddCard (c19);
+			d.AddCard (c20);
+			d.AddCard (c21);
+			d.AddCard (c22);
+			d.AddCard (c23);
+			d.AddCard (c24);
+			d.AddCard (c25);
+			d.AddCard (c26);
+			d.AddCard (c27);
+			d.AddCard (c28);
+			d.AddCard (c29);
+			d.AddCard (c30);
+			d.AddCard (c31);
+			d.AddCard (c32);
+			d.AddCard (c33);
+			d.AddCard (c34);
+			d.AddCard (c35);
+			d.AddCard (c36);
+			d.AddCard (c37);
+			d.AddCard (c38);
+			d.AddCard (c39);
+			d.AddCard (c40);
+			d.AddCard (c41);
+			d.AddCard (c42);
+			d.AddCard (c43);
+			d.AddCard (c44);
+			d.AddCard (c45);
+			d.AddCard (c46);
+			d.AddCard (c47);
+			d.AddCard (c48);
+			d.AddCard (c49);
+			d.AddCard (c50);
+			d.AddCard (c51);
+			d.AddCard (c52);
+
+
+
+
+
+
+
 
 		}
 	}
