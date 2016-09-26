49e49a3395ea4fd4d22fb894bcae95a6ef0cc84b
Ian Young <ianyoung3737@gmail.com>
2016-09-25 20:08:59 -0400

    created bs cards in Card Count Game

--- Homework1/CardCountGame.cs
+++ Homework1/CardCountGame.cs
@@ -12,7 +12,61 @@ namespace Homework1
 		{
 			Deck d = new Deck ();
 
-			Card[] cards = new Card[52];
+			Card c14 = new Card (Suit.CLUBS, Rank.ACE);
+			Card c13 = new Card (Suit.CLUBS, Rank.KING);
+			Card c12 = new Card (Suit.CLUBS, Rank.QUEEN);
+			Card c11 = new Card (Suit.CLUBS, Rank.JACK);
+			Card c10 = new Card (Suit.CLUBS, Rank.TEN);
+			Card c9 = new Card (Suit.CLUBS, Rank.NINE);
+			Card c8 = new Card (Suit.CLUBS, Rank.EIGHT);
+			Card c7 = new Card (Suit.CLUBS, Rank.SEVEN);
+			Card c6 = new Card (Suit.CLUBS, Rank.SIX);
+			Card c5 = new Card (Suit.CLUBS, Rank.FIVE);
+			Card c4 = new Card (Suit.CLUBS, Rank.FOUR);
+			Card c3 = new Card (Suit.CLUBS, Rank.THREE);
+			Card c2 = new Card (Suit.CLUBS, Rank.TWO);
+
+			Card c27 = new Card (Suit.DIAMONDS, Rank.ACE);
+			Card c26 = new Card (Suit.DIAMONDS, Rank.KING);
+			Card c25 = new Card (Suit.DIAMONDS, Rank.QUEEN);
+			Card c24 = new Card (Suit.DIAMONDS, Rank.JACK);
+			Card c23 = new Card (Suit.DIAMONDS, Rank.TEN);
+			Card c22 = new Card (Suit.DIAMONDS, Rank.NINE);
+			Card c21 = new Card (Suit.DIAMONDS, Rank.EIGHT);
+			Card c20 = new Card (Suit.DIAMONDS, Rank.SEVEN);
+			Card c19 = new Card (Suit.DIAMONDS, Rank.SIX);
+			Card c18 = new Card (Suit.DIAMONDS, Rank.FIVE);
+			Card c17 = new Card (Suit.DIAMONDS, Rank.FOUR);
+			Card c16 = new Card (Suit.DIAMONDS, Rank.THREE);
+			Card c15 = new Card (Suit.DIAMONDS, Rank.TWO);
+
+			Card c40 = new Card (Suit.HEARTS, Rank.ACE);
+			Card c39 = new Card (Suit.HEARTS, Rank.KING);
+			Card c38 = new Card (Suit.HEARTS, Rank.QUEEN);
+			Card c37 = new Card (Suit.HEARTS, Rank.JACK);
+			Card c36 = new Card (Suit.HEARTS, Rank.TEN);
+			Card c35 = new Card (Suit.HEARTS, Rank.NINE);
+			Card c34 = new Card (Suit.HEARTS, Rank.EIGHT);
+			Card c33 = new Card (Suit.HEARTS, Rank.SEVEN);
+			Card c32 = new Card (Suit.HEARTS, Rank.SIX);
+			Card c31 = new Card (Suit.HEARTS, Rank.FIVE);
+			Card c30 = new Card (Suit.HEARTS, Rank.FOUR);
+			Card c29 = new Card (Suit.HEARTS, Rank.THREE);
+			Card c28 = new Card (Suit.HEARTS, Rank.TWO);
+
+			Card c53 = new Card (Suit.SPADES, Rank.ACE);
+			Card c52 = new Card (Suit.SPADES, Rank.KING);
+			Card c51 = new Card (Suit.SPADES, Rank.QUEEN);
+			Card c50 = new Card (Suit.SPADES, Rank.JACK);
+			Card c49 = new Card (Suit.SPADES, Rank.TEN);
+			Card c48 = new Card (Suit.SPADES, Rank.NINE);
+			Card c47 = new Card (Suit.SPADES, Rank.EIGHT);
+			Card c46 = new Card (Suit.SPADES, Rank.SEVEN);
+			Card c45 = new Card (Suit.SPADES, Rank.SIX);
+			Card c44 = new Card (Suit.SPADES, Rank.FIVE);
+			Card c43 = new Card (Suit.SPADES, Rank.FOUR);
+			Card c42 = new Card (Suit.SPADES, Rank.THREE);
+			Card c41 = new Card (Suit.SPADES, Rank.TWO);
 
 		}
 	}
